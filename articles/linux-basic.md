c243ed559a995a77d8e692b098380b96ad2e2730
nomikazu-x <nomikazyu@gmail.com>
2024-06-25 11:25:06 +0900

    zenn修正

--- articles/linux-basic.md
+++ articles/linux-basic.md
@@ -6,6 +6,7 @@ topics:
   - Ruby
   - Ruby on Rails
   - Rspec
+  - テスト
 published: true
 ---
 
--- upload.sh
+++ upload.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-FILE=${1}
+# 1. .md形式の変更があったファイルのパスとファイル名を取得
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep '\.md$')
 
-# ファイルのパスがarticles配下ではないまたはファイルが存在しない場合、処理をスキップ
-if [[ ! $FILE =~ ^articles/ ]] || [[ ! -e $FILE ]]; then
-    echo "Skip ztoq: $FILE"
-    continue
-fi
+# 2. & 3. 変更があった.mdファイルごとに指定されたコマンドを実行
+for FILE in $CHANGED_FILES; do
+    # 変更があったファイルのパスを表示
+    echo "Changed file path: $FILE"
+    
+    # ファイルのパスがarticles配下ではない場合、処理をスキップ
+    if [[ ! $FILE =~ ^articles/ ]]; then
+        continue
+    fi
+    
+    # .md拡張子を削除してファイル名を取得
+    FILENAME=$(basename $FILE .md)
+    
+    # ファイル名と同じファイルがqiita/public配下に存在しない場合だけ、npx qiita newを実行
+    if [ ! -e "qiita/public/$FILENAME.md" ]; then
+        cd ./qiita
+        npx qiita new "$FILENAME"
+        cd ../
+    fi
 
-# .md拡張子を削除してファイル名を取得
-FILENAME=$(basename $FILE .md)
+    # ./node_modules/.bin/ts-node scripts/ztoq.ts "取得したファイルパス" qiita/public/"取得したファイル名.md"を実行
+    ./node_modules/.bin/ts-node scripts/ztoq.ts "$FILE" "qiita/public/$FILENAME.md"
+done
 
-# ファイル名と同じファイルがqiita/public配下に存在しない場合だけ、npx qiita newを実行
-if [ ! -e "qiita/public/$FILENAME.md" ]; then
-    echo "Create qiita/public/$FILENAME.md"
-    cd ./qiita
-    npx qiita new "$FILENAME"
-    cd ../
-fi
-
-# ./node_modules/.bin/ts-node scripts/ztoq.ts "取得したファイルパス" qiita/public/"取得したファイル名.md"を実行
-./node_modules/.bin/ts-node scripts/ztoq.ts "$FILE" "qiita/public/$FILENAME.md"
-echo "Convert $FILE to qiita/public/$FILENAME.md"
+# 最後にgit hubにあげる
+git add .
+git commit -m "保存"
+git push
